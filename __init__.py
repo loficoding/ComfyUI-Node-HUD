587f36fc3632fc1f8c7666103b4b1592b6de81d6
loficoding <loficoding87@gmail.com>
2026-01-24 10:53:25 -0700

    Added json file for comfyui manager information regarding version number, author, etc. Fixed "import failed" error in ComfyUI manager.

--- __init__.py
+++ __init__.py
@@ -1,2 +1,7 @@
-WEB_DIRECTORY = "./web" 
-__all__ = ["WEB_DIRECTORY"]
+# Mappings required for ComfyUI to acknowledge the node folder
+NODE_CLASS_MAPPINGS = {}
+NODE_DISPLAY_NAME_MAPPINGS = {}
+
+WEB_DIRECTORY = "./web"
+
+__all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS", "WEB_DIRECTORY"]
